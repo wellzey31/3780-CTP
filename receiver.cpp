77ea8980d570c56088f8abecdf0424ac096f1608
brett <brettwwells@gmail.com>
2021-03-14 14:54:51 -0600

    test linux connection to repo

--- receiver.cpp
+++ receiver.cpp
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[]) {
   std::cout << "argc: " << argc << std::endl;
 
   for (int i = 0; i < argc; i++) {
-    std::cout << argv[i] << std::endl;
+    std::cout << argv[i] << std::endl;//hello
   }
 
  return 0;
