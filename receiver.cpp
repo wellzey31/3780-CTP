266f8845703ec1b35f431dc7a0c04e9dd01386c8
Wells <brett.wells@uleth.ca>
2021-03-14 13:02:45 -0600

    push some empty files

---
+++ receiver.cpp
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+
+ return 0;
+}
---
+++ sender.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
