8624caa61f1372dc2ddd50b0038c2d5d1ad34db2
brett <brettwwells@gmail.com>
2021-03-14 17:05:54 -0600

    receiver is working

--- receiver.cpp
+++ receiver.cpp
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include <string.h>
 #include <iostream>
 #include <cstring>
@@ -27,8 +28,8 @@ int main(int argc, char const *argv[]) {
         std::cout << " port: " << port << " file: " << file << std::endl;
     }
 
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == )) {
-      std::cerr >> "Socket failed" << std::endl;
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == 0 ) {
+      std::cerr << "Socket failed" << std::endl;
       exit(EXIT_FAILURE);
     }
 
@@ -37,31 +38,33 @@ int main(int argc, char const *argv[]) {
       exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    unsigned short portnum = (unsigned short) std::strtoul(port.c_str(), NULL, 0);
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = INADDR_ANY;
+    sa.sin_port = htons( portnum );
 
     // Forcefully attaching socket to the port 8080
     if (bind(s, (struct sockaddr *)&sa,
                                  sizeof(sa))<0)
     {
-        perror("bind failed");
+        std::cerr << "bind failed" << std::endl;
         exit(EXIT_FAILURE);
     }
     if (listen(s, 3) < 0)
     {
-        perror("listen");
+        std::cerr << "listen" << std::endl;
         exit(EXIT_FAILURE);
     }
     if ((new_socket = accept(s, (struct sockaddr *)&sa,
                        (socklen_t*)&addrlen))<0)
     {
-        perror("accept");
+        std::cerr << "accept" << std::endl;
         exit(EXIT_FAILURE);
     }
     valread = read( new_socket , buffer, 1024);
     printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
+    send(new_socket , message , strlen(message) , 0 );
     printf("Mensaje de servidor enviado\n");
 
  return 0;
