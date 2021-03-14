90364f0dd5522960f8efd9b804987cda1aeeae83
Joshua Carruthers <mozzie1706@gmail.com>
2021-03-14 15:58:50 -0600

    Added socket creation

--- sender.cpp
+++ sender.cpp
@@ -9,9 +9,10 @@
 #define PORT 8080
 
 int main(int argc, char const *argv[]) {
+  int s;
 
   struct sockaddr_in sa;
-  struct addrinfo hints;
+  struct addrinfo hints, *res;
   std::string host, port, file;
 
   memset(&hints, 0, sizeof hints); //makes sure the struct is empty
@@ -23,16 +24,24 @@ int main(int argc, char const *argv[]) {
         std::cerr << "[ERROR] incorrect arguments.";
         std::cerr << "usage: sender -f <file> <ip> <port>\n";
         exit(1);
-    }
-
-    std::cout << argv[1] << std::endl;
-
-  if (argc == 5) { //&& argv[1] == "-f") {
+  }
+  else if (argc == 5) { //&& argv[1] == "-f") {
     host = argv[3];
     file = argv[2];
     port = argv[4];
     std::cout << "host: " << host << " port: " << port << " file: " << file << std::endl;
   }
+
+  getaddrinfo(host.c_str(), port.c_str(), &hints, &res);
+
+  s = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
+
+  if(s == -1) {
+    std::cerr << "[ERROR] socket = -1.";
+    exit(1);
+  }
+
+  std::cout << argv[1] << std::endl;
 
   inet_pton(AF_INET, host.c_str(), &(sa.sin_addr));
 
