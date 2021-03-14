5b4d5b8025b8b7f0d478f97adb6d747041db800c
Wells <brett.wells@uleth.ca>
2021-03-14 13:26:19 -0600

    test argc value

--- sender.cpp
+++ sender.cpp
@@ -6,6 +6,13 @@
 
 int main(int argc, char const *argv[]) {
 
+  if (argc < 3 || argc > 5) {
+        printf("usage: sender -f <file> <ip> <port>\n");
+        exit(1);
+    }
+
+  std::cout << "argc: " << argc << std::endl;
+
   for (int i = 0; i < argc; i++) {
     std::cout << argv[i] << std::endl;
   }
