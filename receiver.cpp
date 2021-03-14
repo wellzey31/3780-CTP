87dab8bc7101a72a8354d537b5f09f98368fe9b0
Wells <brett.wells@uleth.ca>
2021-03-14 13:34:26 -0600

    add/test receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -1,11 +1,21 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
+//#include <sys/socket.h>
 #include <string.h>
 #include <iostream>
 #define PORT 8080
 
 int main(int argc, char const *argv[]) {
+
+  if (argc < 3 || argc > 5) {
+        printf("usage: sender -f <file> <ip> <port>\n");
+        exit(1);
+    }
+
+  std::cout << "argc: " << argc << std::endl;
+
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << std::endl;
+  }
 
  return 0;
 }
