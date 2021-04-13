725f8a564c2bf2c1670bd888cdfa37772e2c2fce
brett <brettwwells@gmail.com>
2021-04-12 21:47:52 -0600

    push

--- receiver.cpp
+++ receiver.cpp
@@ -18,9 +18,9 @@ void send_ack(SimpleHeader* read, int s) {
   SimpleHeader* ack = new SimpleHeader();
 
   std::bitset<32> readCRC(read->thePacket().crc1.to_ulong());
-  //read->setCRC1();
-  std::cerr << readCRC << std::endl;
-  std::cerr << read->thePacket().crc1 << std::endl;
+  read->setCRC1();
+  //std::cerr << readCRC << std::endl;
+  //std::cerr << read->thePacket().crc1 << std::endl;
 
   if (read->thePacket().crc1 == readCRC) {
     ack -> setType(2);
